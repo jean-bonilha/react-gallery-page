8a0c6e1c5b9ddd529d86e3c6705a2b2fbb4b3b14
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2022-03-04 23:51:59 -0400

    develop(C4):create Image.js component

---
+++ src/Image.js
@@ -0,0 +1,16 @@
+import { ImageViewer } from "react-image-viewer-dv"
+
+function Image({ image }) {
+  return (
+    <div className="image">
+      <ImageViewer className="image-header">
+        <img
+          src={image.url}
+          alt={image.title}
+        />
+      </ImageViewer>
+    </div>
+  );
+}
+
+export default Image;
